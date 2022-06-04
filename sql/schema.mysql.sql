b57db828b570065ee483dba1505e761f985104ef
Kelvin Welter <kelvinwelter.b@gmail.com>
2022-06-04 15:51:59 -0300

    setup PlanetScale DB

--- sql/schema.mysql.sql
+++ sql/schema.mysql.sql
@@ -20,8 +20,7 @@ create table website (
     name varchar(100) not null,
     domain varchar(500),
     share_id varchar(64) unique,
-    created_at timestamp default current_timestamp,
-    foreign key (user_id) references account(user_id) on delete cascade
+    created_at timestamp default current_timestamp
 ) ENGINE=InnoDB COLLATE=utf8_general_ci;
 
 create table session (
@@ -35,8 +34,7 @@ create table session (
     device varchar(20),
     screen varchar(11),
     language varchar(35),
-    country char(2),
-    foreign key (website_id) references website(website_id) on delete cascade
+    country char(2)
 ) ENGINE=InnoDB COLLATE=utf8_general_ci;
 
 create table pageview (
@@ -45,9 +43,7 @@ create table pageview (
     session_id int unsigned not null,
     created_at timestamp default current_timestamp,
     url varchar(500) not null,
-    referrer varchar(500),
-    foreign key (website_id) references website(website_id) on delete cascade,
-    foreign key (session_id) references session(session_id) on delete cascade
+    referrer varchar(500)
 ) ENGINE=InnoDB COLLATE=utf8_general_ci;
 
 create table event (
@@ -57,9 +53,7 @@ create table event (
     created_at timestamp default current_timestamp,
     url varchar(500) not null,
     event_type varchar(50) not null,
-    event_value varchar(50) not null,
-    foreign key (website_id) references website(website_id) on delete cascade,
-    foreign key (session_id) references session(session_id) on delete cascade
+    event_value varchar(50) not null
 ) ENGINE=InnoDB COLLATE=utf8_general_ci;
 
 create index website_user_id_idx on website(user_id);
